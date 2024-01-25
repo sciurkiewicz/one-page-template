d7279d778db689fb44d77c772d5e0caa926616b5
Szymon Ciurkiewicz <65823135+sciurkiewicz@users.noreply.github.com>
2024-01-25 17:00:10 +0100

    changes

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 	<div class="w-full md:max-w-4xl mx-auto flex flex-wrap items-center justify-between mt-0 py-3">
 		<div class="block lg:hidden pl-8 p-2">
 			<a class="text-yellow-300 font-bold text-base no-underline hover:no-underline" href="#">
-				TechFest
+				Szymon Ciurkiewicz
 			</a>
 		</div>
 		<div class="block lg:hidden pr-4">
@@ -45,8 +45,8 @@
 	<div class="bg-black text-white h-screen align-middle">
 		<div class="container mx-auto flex flex-col md:flex-row items-center align-middle flex justify-center items-center h-screen">
 			<div class="flex flex-col w-full lg:w-1/3 justify-center items-start p-8">
-				<h1 class="text-3xl md:text-5xl mb-3 text-yellow-300 tracking-loose">Szymon Ciurkiewicz</h1>
-				<h2 class="text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2">Front-End Developer
+				<h1 class="text-3xl md:text-8xl mb-3 text-yellow-300 tracking-loose" style="font-weight: 700">Szymon Ciurkiewicz</h1>
+				<h2 class="text-3xl md:text-3xl leading-relaxed md:leading-snug mb-2">Front-End Developer
 				</h2>
 				<p class="text-sm md:text-base text-gray-50 mb-4">Explore your favourite events and
 					register now to showcase your talent and win exciting prizes.</p>
@@ -67,8 +67,7 @@
 
 <div class="mx-auto flex flex-col md:flex-row">
 	<div class="flex flex-col w-full lg:w-3/5 p-8">
-		<h1 class="text-3xl md:text-5xl text-yellow-300 my-4 leading-relaxed md:leading-snug">Skills
-		</h1>
+		<h1 class="text-3xl md:text-8xl mb-3 text-yellow-300 tracking-loose" style="font-weight: 700">Skills</h1>
 		<div class="font-sans text-sm md:text-lg my-2 md:my-4">Our team has put a lot of effort to build this
 			website
 			and get a platform for people to register for this overall India tech fest. We really appreciate
@@ -156,29 +155,46 @@
 		    <div class="bg-black text-white py-8">
     <div class="container mx-auto flex flex-col items-start md:flex-row my-12 md:my-24">
       <div class="flex flex-col w-full sticky md:top-36 lg:w-1/3 mt-2 md:mt-12 px-8">
-			<h1 class="text-3xl md:text-5xl text-yellow-300 my-4 leading-relaxed md:leading-snug">Projects
-			</h1>
-			<div class="font-sans text-sm md:text-lg my-2 md:my-4">Our team has put a lot of effort to build this
-				website
-				and get a platform for people to register for this overall India tech fest. We really appreciate
-				each team member's contribution and dedication towards the website.
-				Do leave a feedback if you enjoyed being here. <br>
-				<p>Regards,</p>
-				<p>Team i-GNEz</p>
+		<h1 class="text-3xl md:text-6xl mb-3 text-yellow-300 tracking-loose" style="font-weight: 700">Contact me 👇</h1>
+			<p class="font-sans text-sm md:text-lg my-2 md:my-4"><svg class="inline-block fill-current mr-2"
+            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path
+              d="M12 12.713l-11.985-9.713h23.971l-11.986 9.713zm-5.425-1.822l-6.575-5.329v12.501l6.575-7.172zm10.85 0l6.575 7.172v-12.501l-6.575 5.329zm-1.557 1.261l-3.868 3.135-3.868-3.135-8.11 8.848h23.956l-8.11-8.848z" />
+          </svg> sciurkiewicz@gmail.com</p>
+			<div class="font-sans text-sm md:text-lg my-2 md:my-4">
+				<h1></h1>
 			</div>
 			<div class="font-sans text-sm md:text-lg my-2 md:my-4">
-				
-				<p>Regards,</p>
-				<p>Team i-GNEz</p>
+				<form id="feedbackForm" action="" method="">
+										<div class="relative w-full mb-3">
+											<label class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                        for="email">Email</label><input type="email" name="email" id="email" class="border-0 px-3 py-3 rounded text-sm shadow w-full
+                    bg-gray-300 placeholder-black text-gray-800 outline-none focus:bg-gray-400" placeholder=" "
+                        style="transition: all 0.15s ease 0s;" required />
+                    </div>
+											<div class="relative w-full mb-3">
+												<label class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                        for="message">Message</label><textarea maxlength="300" name="feedback" id="feedback" rows="4"
+                        cols="80"
+                        class="border-0 px-3 py-3 bg-gray-300 placeholder-black text-gray-800 rounded text-sm shadow focus:outline-none w-full"
+                        placeholder="" required></textarea>
+											</div>
+											<div class="text-center mt-6">
+												<button id="feedbackBtn"
+                        class="bg-yellow-300 text-black text-center mx-auto active:bg-yellow-400 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                        type="submit" style="transition: all 0.15s ease 0s;">Submit
+                      </button>
+											</div>
+									</form>
 			</div>
       </div>
       <div class="ml-0 md:ml-12 lg:w-2/3 sticky">
         <div class="container mx-auto w-full h-full">
           <div class="relative wrap overflow-hidden p-10 h-full">
-            <div class="border-2-2 border-yellow-555 absolute h-full border"
-              style="right: 50%; border: 2px solid #FFC100; border-radius: 1%;"></div>
-            <div class="border-2-2 border-yellow-555 absolute h-full border"
-              style="left: 50%; border: 2px solid #FFC100; border-radius: 1%;"></div>
+            <div class="border-2-2 border-yellow-300 absolute h-full border"
+              style="right: 50%; border: 2px solid rgb(253 224 71);; border-radius: 1%;"></div>
+            <div class="border-2-2 border-yellow-300 absolute h-full border"
+              style="left: 50%; border: 2px solid rgb(253 224 71);; border-radius: 1%;"></div>
             <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
               <div class="order-1 w-5/12"></div>
               <div class="order-1 w-5/12 px-1 py-4 text-right">
@@ -202,6 +218,30 @@
               </div>
             </div>
             <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+              <div class="order-1 w-5/12"></div>
+              <div class="order-1 w-5/12 px-1 py-4 text-right">
+                <p class="mb-3 text-base text-yellow-300">JavaScript/Electron</p>
+                <h4 class="mb-3 font-bold text-lg md:text-2xl">Result Declaration</h4>
+                <p class="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
+                  The ultimate genius will be revealed by our judging panel on 10th May, 2021 and the resukts will be
+                  announced on the whatsapp groups and will be mailed to you.
+                </p>
+              </div>
+            </div>
+
+            <div class="mb-8 flex justify-between items-center w-full right-timeline">
+              <div class="order-1 w-5/12"></div>
+
+              <div class="order-1  w-5/12 px-1 py-4">
+                <p class="mb-3 text-base text-yellow-300">JavaScript/Electron</p>
+                <h4 class="mb-3 font-bold  text-lg md:text-2xl text-left">Prize Distribution</h4>
+                <p class="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
+                  The winners will be contacted by our team for their addresses and the winning goodies will be sent at
+                  their addresses.
+                </p>
+              </div>
+            </div>
+			<div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
               <div class="order-1 w-5/12"></div>
               <div class="order-1 w-5/12 px-1 py-4 text-right">
                 <p class="mb-3 text-base text-yellow-300">JavaScript/Electron</p>
